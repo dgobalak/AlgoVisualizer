e2aecacac0114ab6f1de11764be0c7dad8c425d1
dgobalak <dgobalak@uwaterloo.ca>
2021-12-05 02:34:33 -0500

    Implemented selection sort

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from settings import *
+# from sorting_algos.selection_sort import *
 import numpy as np
 import pygame
 import random
@@ -11,44 +12,64 @@ clock = pygame.time.Clock()
 
 matrix = np.ones((NUM_ROWS, NUM_COLS))
 
+ALGO = "SORT"   # Or "PATH"
+
 # FOR TESTING
 # matrix[-1] = np.zeros((len(matrix[0])))
 # matrix[:, -1] = np.zeros((len(matrix[:, 0])))
+
+def matrix_from_nums(nums, matrix):
+    for col in range(np.shape(matrix)[1]):
+        num = nums[col]
+        tmp_col = np.ones(len(matrix[:, col]))
+        tmp_col[(len(tmp_col)-num):(len(tmp_col))] = 0
+        matrix[:, col] = tmp_col
+
 
 def randomize_sorting_matrix(matrix):
     rand_nums = np.arange(1, NUM_COLS + 1, NUM_ROWS//NUM_COLS)
     np.random.shuffle(rand_nums)
     
-    for col in range(np.shape(matrix)[1]):
-        rand_num = rand_nums[col]
-        tmp_col = np.ones(len(matrix[:, col]))
-        tmp_col[(len(tmp_col)-rand_num):(len(tmp_col))] = 0
-        matrix[:, col] = tmp_col
+    matrix_from_nums(rand_nums, matrix)
+    
+    return rand_nums
 
 
-def create_square(x, y, color):
+def draw_node(screen, x, y, color):
     pygame.draw.rect(screen, color, [x, y, NODE_WIDTH, NODE_HEIGHT])
 
 
-randomize_sorting_matrix(matrix)
-print(list(matrix))
-## Game loop
-running = True
-while running:
-    screen.fill(BLACK)
-    clock.tick(FPS)   
-     
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            
+def draw_matrix(screen, matrix):
     for row in range(np.shape(matrix)[0]):
         for col in range(np.shape(matrix)[1]):
             node = matrix[row, col]
             if node == 0:
-                create_square(col*NODE_WIDTH, row*NODE_HEIGHT, WHITE)
+                draw_node(screen, col*NODE_WIDTH, row*NODE_HEIGHT, WHITE)
             else:
-                create_square(col*NODE_WIDTH, row*NODE_HEIGHT, BLACK)
+                draw_node(screen, col*NODE_WIDTH, row*NODE_HEIGHT, BLACK)
 
-    pygame.display.flip()       
 
+def selection_sort(screen, matrix):
+    nums = randomize_sorting_matrix(matrix)
+    screen.fill(BLACK)
+    clock.tick(FPS)  
+    
+    for i in range(len(nums)):
+        screen.fill(BLACK)
+        clock.tick(FPS)  
+    
+        min_i, min_val = i, nums[i]
+        for j in range(i, len(nums)):
+            if nums[j] <= min_val:
+                min_i, min_val = j, nums[j]
+        nums[i], nums[min_i] = nums[min_i], nums[i]
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        matrix_from_nums(nums, matrix)
+        draw_matrix(screen, matrix)
+        pygame.display.flip()    
+
+selection_sort(screen, matrix)
