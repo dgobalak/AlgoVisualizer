c86edbd1d8d98c9d37ad274e61df60e78600ff56
dgobalak <dgobalak@uwaterloo.ca>
2021-12-05 01:13:35 -0500

    Replaced visualizer with pygame and made settings

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
+from settings import *
+import numpy as np
+import pygame
 import random
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-from itertools import count
-import sys
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption(TITLE)
+clock = pygame.time.Clock()
+
+matrix = np.ones((NUM_ROWS, NUM_COLS))
+
+# FOR TESTING
+# matrix[-1] = np.zeros((len(matrix[0])))
+# matrix[:, -1] = np.zeros((len(matrix[:, 0])))
+
+def createSquare(x, y, color):
+    pygame.draw.rect(screen, color, [x, y, NODE_WIDTH, NODE_HEIGHT])
 
 
-x_axis = [x for x in range(50)]
-y_axis = [y for y in range(50)]
-random.shuffle(y_axis)
+## Game loop
+running = True
+while running:
+    screen.fill(BLACK)
+    clock.tick(FPS)    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+    for row in range(np.shape(matrix)[0]):
+        for col in range(np.shape(matrix)[1]):
+            node = matrix[row, col]
+            if node == 0:
+                createSquare(col*NODE_WIDTH, row*NODE_HEIGHT, WHITE)
+            else:
+                createSquare(col*NODE_WIDTH, row*NODE_HEIGHT, BLACK)
 
-c = count()
+    pygame.display.flip()       
 
-def animate(a):
-    if a == len(y_axis):
-        sys.exit()
-    plt.clf()
-    bar = plt.bar(x_axis, y_axis,color="black", edgecolor="black")
-    selection_sort(y_axis, a, bar)
-
-
-def selection_sort(y_axis, a, bar):
-    bar[a].set_color('red')
-    bar[a].set_edgecolor('red')
-    min_i, min_val = a, 1E10
-    for i in range(a, len(y_axis)):
-        if y_axis[i] <= min_val:
-            min_val = y_axis[i]
-            min_i = i
-    y_axis[min_i], y_axis[a] = y_axis[a], y_axis[min_i]
-    bar[min_i].set_color('green')
-    bar[min_i].set_edgecolor('green')
-
-
-ani = FuncAnimation(plt.gcf(), animate, blit=False, frames=c, repeat=True)
-plt.show()
