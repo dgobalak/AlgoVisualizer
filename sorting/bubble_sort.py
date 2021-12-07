28311ea6afd80d6a3342d31b3ce5d43724ceac48
dgobalak <dgobalak@uwaterloo.ca>
2021-12-06 21:00:03 -0500

    Exit bubble sort once list is sorted.

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -6,16 +6,25 @@ def bubble_sort(screen, matrix, clock):
     screen.fill(BLACK)
     clock.tick(FPS)
 
+    sorted = False
     for i in range(len(nums)-1):
         screen.fill(BLACK)
         clock.tick(FPS)
-
+        
+        if sorted:
+            matrix_from_nums(nums, matrix)
+            draw_matrix(screen, matrix)
+            pygame.display.flip()
+            break
+        
+        sorted = True
         for j in range(0, len(nums)-i-1):
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
 
             if nums[j+1] < nums[j]:
+                sorted = False
                 nums[j], nums[j+1] = nums[j+1], nums[j]
 
             matrix_from_nums(nums, matrix)
